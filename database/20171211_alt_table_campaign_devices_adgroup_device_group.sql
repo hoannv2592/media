119d0ecbf8fd0c7f6521f9899d61ab161118152f
hoannv <vanhoanpt92@gmail.com>
2017-12-12 01:35:19 +0700

    add database

--- database/20171211_alt_table_campaign_devices_adgroup_device_group.sql
+++ database/20171211_alt_table_campaign_devices_adgroup_device_group.sql
@@ -18,3 +18,9 @@ ALTER TABLE adgroups  ADD  `image_logo` varchar(500) DEFAULT NULL;
 ALTER TABLE adgroups  ADD  `title_connect` varchar(255) DEFAULT NULL;
 ALTER TABLE adgroups  ADD  `hidden_connect` int(12) DEFAULT NULL;
 
+ALTER TABLE devices  ADD `tile_congratulations_return` varchar(500) DEFAULT NULL;
+ALTER TABLE adgroups  ADD `tile_congratulations_return` varchar(500) DEFAULT NULL;
+ALTER TABLE device_groups  ADD `tile_congratulations_return` varchar(500) DEFAULT NULL;
+ALTER TABLE campaign_groups  ADD `tile_congratulations_return` varchar(500) DEFAULT NULL;
+
+
