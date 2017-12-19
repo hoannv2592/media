b6d4fb230d9c70e23cbceacad99bbfd6edfe6ce8
hoannv2592 <vanhoanpt92@gmail.com>
2017-12-19 15:11:06 +0700

    fix

--- config/app.php
+++ config/app.php
@@ -231,7 +231,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => '',
-            'database' => 'media',
+            'database' => 'crm_wifimedia',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
